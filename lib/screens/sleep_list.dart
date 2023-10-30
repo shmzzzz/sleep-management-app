da418171218658f1a127e36f7be9d350274b76f9
shmzzzz <slt0623@gmail.com>
2023-10-31 08:28:34 +0900

    💄 デザインを整える

--- lib/screens/sleep_list.dart
+++ lib/screens/sleep_list.dart
@@ -5,9 +5,33 @@ class SleepListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> list = ['6:23', '7:08', '5:24'];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('睡眠時間一覧'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      body: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: const Icon(
+                Icons.bed,
+                color: Colors.black,
+              ),
+              title: Text(
+                list[index],
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_circle_sharp),
       ),
     );
   }
