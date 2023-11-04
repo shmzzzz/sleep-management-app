0e0ae223bf7a877785ad0b87f8ccae75c9901c15
shmzzzz <slt0623@gmail.com>
2023-11-04 16:44:13 +0900

    ✨ 🛂 認証情報をFirebase Authenticationに保存できるようにする

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sleep_management_app/screens/sleep_list.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -14,6 +12,46 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  // ログイン状態を管理するフラグ
+  var _isLogin = true;
+  // 入力されたメールアドレス
+  var _enteredEmail = '';
+  // 入力されたパスワード
+  var _enteredPassword = '';
+
+  // ログイン処理を行うメソッド
+  void _submit() async {
+    final isValid = _formKey.currentState!.validate();
+
+    // currentStateがvalidate出ない場合は処理を行わない
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+
+    if (_isLogin) {
+      // ログインしている場合の処理
+    } else {
+      try {
+        final userCredentails = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          // ...
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentication failed'),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +73,9 @@ class _AuthScreenState extends State<AuthScreen> {
                   label: Text('メールアドレス'),
                   prefixIcon: Icon(Icons.mail_outline),
                 ),
+                onSaved: (newValue) {
+                  _enteredEmail = newValue!;
+                },
               ),
               const SizedBox(height: 16),
               TextFormField(
@@ -43,23 +84,27 @@ class _AuthScreenState extends State<AuthScreen> {
                   label: Text('パスワード'),
                   prefixIcon: Icon(Icons.key),
                 ),
+                onSaved: (newValue) {
+                  _enteredPassword = newValue!;
+                },
               ),
               const SizedBox(height: 16),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    CupertinoPageRoute(
-                      builder: (context) {
-                        return const SleepListScreen();
-                      },
-                    ),
-                  );
-                },
-                child: const Text('ログイン'),
+                onPressed: _submit,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Theme.of(context).colorScheme.primaryContainer,
+                ),
+                child: Text(_isLogin ? 'ログイン' : '新規作成'),
               ),
               TextButton(
-                onPressed: () {},
-                child: const Text('アカウント新規作成'),
+                onPressed: () {
+                  // このボタンをタップすると_isLoginの値が変わるようにする
+                  setState(() {
+                    _isLogin = !_isLogin;
+                  });
+                },
+                child: Text(_isLogin ? 'アカウント新規作成' : 'ログイン'),
               ),
             ],
           ),
