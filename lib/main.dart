b820e7baa0490ebe1cc5aa02e3bebea92463f40a
shmzzzz <slt0623@gmail.com>
2023-11-05 09:23:39 +0900

    🛂 認証状態を監視して画面を切り替える

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sleep_management_app/screens/auth.dart';
+import 'package:sleep_management_app/screens/sleep_list.dart';
 
 import 'firebase_options.dart';
 
@@ -26,8 +28,22 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 14, 218, 208),
         ),
       ),
-      // home: const SleepListScreen(),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        // 認証状態の変更を取得する
+        // ①リスナーの登録直後 ②ユーザーがログインした時 ③現在のユーザーがログアウトした時
+        // https://firebase.google.com/docs/auth/flutter/start?hl=ja#authstatechanges
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // snapshotがnullでない=ログイン済みの状態
+            // 一覧画面へ遷移する
+            return const SleepListScreen();
+          }
+          // snapshotがnull=未ログイン状態
+          // 認証画面へ遷移する
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
