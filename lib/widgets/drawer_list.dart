afd2b2602fb96549e11bef66ab6418ffed2f7bcb
shmzzzz <slt0623@gmail.com>
2023-11-19 14:09:30 +0900

    💄 ✨ ドロワーを追加する

---
+++ lib/widgets/drawer_list.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sleep_management_app/screens/sleep_add.dart';
+import 'package:sleep_management_app/screens/sleep_list.dart';
+
+class DrawerList extends StatelessWidget {
+  const DrawerList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        children: [
+          SizedBox(
+            height: 120,
+            child: DrawerHeader(
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+              child: const Text(
+                'メニュー',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 4,
+            ),
+            child: ListTile(
+              title: const Text('睡眠データ一覧'),
+              onTap: () {
+                Navigator.of(context).push(
+                  CupertinoPageRoute(
+                    builder: (context) {
+                      return const SleepListScreen();
+                    },
+                  ),
+                );
+              },
+              trailing: const Icon(Icons.arrow_circle_right_outlined),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 4,
+            ),
+            child: ListTile(
+              title: const Text('睡眠データ追加'),
+              onTap: () {
+                Navigator.of(context).push(
+                  CupertinoPageRoute(
+                    builder: (context) {
+                      return const SleepAddScreen();
+                    },
+                  ),
+                );
+              },
+              trailing: const Icon(Icons.arrow_circle_right_outlined),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
