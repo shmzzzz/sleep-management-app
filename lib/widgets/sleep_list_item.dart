ac2ae507ba6f748f86321fc31afc1a89095c17db
shmz <85086833+shmzzzz@users.noreply.github.com>
2023-11-20 10:03:26 +0900

    feat: データが目標睡眠時間を達成しているかわかるように表示する (#16)
    * 🐛 deleteができなくなっていたため修正
    * ✨ 目標の入力、更新ができるようにする
    * ✨ intlパッケージを追加してDateFormatで比較できるようにする
    * ✨ データ追加/更新した場合に目標と比較してアイコンを変更する

--- lib/widgets/sleep_list_item.dart
+++ lib/widgets/sleep_list_item.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sleep_management_app/screens/sleep_edit.dart';
 
-class SleepListItem extends StatelessWidget {
+class SleepListItem extends StatefulWidget {
   const SleepListItem({
     super.key,
     required this.itemData,
@@ -14,47 +16,82 @@ class SleepListItem extends StatelessWidget {
   final String documentId;
 
   @override
+  State<SleepListItem> createState() => _SleepListItemState();
+}
+
+class _SleepListItemState extends State<SleepListItem> {
+  bool isAchieved = false;
+
+  void updateAchievment() {
+    DateTime totalDateTime =
+        DateFormat('HH:mm').parse(widget.itemData['total']);
+    DateTime goalDateTime = DateFormat('HH:mm').parse(widget.itemData['goal']);
+    // 目標との比較
+    setState(() {
+      isAchieved = totalDateTime.isAfter(goalDateTime) ||
+          totalDateTime.isAtSameMomentAs(goalDateTime);
+    });
+  }
+
+  void showSnackBar(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  void deleteData(String documentId) async {
+    try {
+      // ユーザーのUIDを取得
+      String userUid = FirebaseAuth.instance.currentUser!.uid;
+      // ユーザーごとにデータを保存するパスを構築
+      String userPath = 'users/$userUid/data';
+      await FirebaseFirestore.instance
+          .collection(userPath)
+          .doc(documentId)
+          .delete();
+    } catch (error) {
+      showSnackBar(error.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updateAchievment();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final total = itemData['total'];
-    final sleep = itemData['sleep'];
-    final core = itemData['core'];
-
-    void showSnackBar(String message) {
-      ScaffoldMessenger.of(context).clearSnackBars();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-        ),
-      );
-    }
-
-    void deleteData(String documentId) async {
-      try {
-        await FirebaseFirestore.instance
-            .collection('sleep-data')
-            .doc(documentId)
-            .delete();
-      } catch (error) {
-        showSnackBar(error.toString());
-      }
-    }
+    final total = widget.itemData['total'];
+    final sleep = widget.itemData['sleep'];
+    final core = widget.itemData['core'];
 
     return Card(
       elevation: 5,
       child: ListTile(
-        onTap: () {
-          Navigator.of(context).push(
+        onTap: () async {
+          var result = await Navigator.of(context).push(
             CupertinoPageRoute(
               builder: (context) {
                 return SleepEditScreen(
-                  initialData: itemData,
-                  documentId: documentId,
+                  initialData: widget.itemData,
+                  documentId: widget.documentId,
                 );
               },
             ),
           );
+
+          // データが更新された場合はisAchievedが変わる可能性がある
+          if (result != null) {
+            isAchieved = result;
+          }
         },
-        leading: const Icon(Icons.bed),
+        leading: isAchieved
+            ? const Icon(Icons.check_circle_outline_outlined)
+            : const Icon(Icons.bed),
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -93,7 +130,7 @@ class SleepListItem extends StatelessWidget {
         trailing: IconButton(
           icon: const Icon(Icons.delete),
           onPressed: () {
-            deleteData(documentId);
+            deleteData(widget.documentId);
           },
         ),
       ),
